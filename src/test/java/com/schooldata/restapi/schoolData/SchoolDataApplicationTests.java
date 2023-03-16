c660745328dd6ee73b3895d23396fce38d97c3b2
Ganesh <ganeshyadv1997@gmail.com>
2023-03-16 19:54:19 +0530

    Initial Commit

---
+++ src/test/java/com/schooldata/restapi/schoolData/SchoolDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schooldata.restapi.schoolData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
